5f3cdeb0ee1163ba6484eb927aa1450a33d00c10
Garima <garima@punchh.com>
2015-05-06 15:09:21 +0530

    optimized code

--- app/src/main/java/test/git/com/gittest/MainActivity.java
+++ app/src/main/java/test/git/com/gittest/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // This is minor change.
+        // Second Change.
     }
 
     @Override
